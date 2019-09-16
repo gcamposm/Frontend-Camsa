27be3a12b5adb7952dd9cb2cff7941d4bbd035b1
Guillermo Campos <guillermo.campos@usach.cl>
2019-09-16 03:25:27 -0300

    Fix: Server URL

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        serverURL: 'http://localhost:8080',
-        //serverURL: 'http://104.248.179.110:8080/camsa',
+        //serverURL: 'http://localhost:8080',
+        serverURL: 'http://104.248.179.110:8080/camsa',
     }
 })
